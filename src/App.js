4db337f9d59c8cb09c5273c647e6d3820fa20f34
shivaorsu <shivaorsu785@gmail.com>
2022-12-11 16:49:58 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import { useState,useContext, useEffect } from "react";
-import { Redirect,Route, Switch } from "react-router-dom";
-import React,{Suspense} from "react";
+import { useState, useContext, useEffect } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import React, { Suspense } from "react";
 import Cart from "./Compontents/Cart/Cart";
 import Footer from "./Compontents/Layout/Footer";
 import Header from "./Compontents/Layout/Header";
 //import Cart from "./Compontents/Cart/Cart";
-import Products from "./Compontents/Products/Products";
+//import Products from "./Compontents/Products/Products";
 //import Home from "./Compontents/Homee/Home";
-import About from "./Compontents/About/About";
+//import About from "./Compontents/About/About";
 import CartProvider from "./Compontents/Store/CartProvider";
-import Contact from "./Compontents/Contact/Contact";
+//import Contact from "./Compontents/Contact/Contact";
 import classes from "./Compontents/Layout/Header.module.css";
 import ProductDetail from "./Compontents/Products/ProductDetails";
 import AuthContext from "./Compontents/Store/AuthContext";
 import AuthForm from "./Compontents/Authentication/authForm";
 import CartContext from "./Compontents/Store/cart-context";
 import axios from "axios";
+import LoadingSpinner from "./Compontents/UI/LoadingSpinner";
 
-const Home = React.lazy(()=>import ('./Compontents/Homee/Home'));
+const Home = React.lazy(() => import("./Compontents/Homee/Home"));
+const Contact = React.lazy(()=> import ("./Compontents/Contact/Contact"));
+const Products = React.lazy(()=> import ("./Compontents/Products/Products"));
+const About = React.lazy(()=> import ("./Compontents/About/About"));
 
 
 function App() {
+  const cartCtx = useContext(CartContext);
 
-  const cartCtx= useContext(CartContext)
-  
-  if(!localStorage.getItem('email')) {
-    localStorage.setItem("email","")
+  if (!localStorage.getItem("email")) {
+    localStorage.setItem("email", "");
   }
   const authCtx = useContext(AuthContext);
   let email = localStorage.getItem("email").replace(".", "").replace("@", "");
-
-
-
 
   const [cartisShown, setCartIsShown] = useState(false);
 
@@ -42,21 +42,25 @@ function App() {
   const hideCartHandler = () => {
     setCartIsShown(false);
   };
-console.log(authCtx.isLoggedIn)
- useEffect(() => {
-   if (!email) return;
-    axios.get(`https://crudcrud.com/api/faae72b079de4bd79023fcb94065ebe9/cart${email}`).then((res) => {
-      const data= (res.data)
-      for (const key in data) {
-        const item = data[key];
-        item._id = key;
-        cartCtx.mapID(item)
-      }
-      
-    }).catch((err) => {
-      alert(err)
-    })
-  }, [email, cartCtx])
+  console.log(authCtx.isLoggedIn);
+  useEffect(() => {
+    if (!email) return;
+    axios
+      .get(
+        `https://crudcrud.com/api/faae72b079de4bd79023fcb94065ebe9/cart${email}`
+      )
+      .then((res) => {
+        const data = res.data;
+        for (const key in data) {
+          const item = data[key];
+          item._id = key;
+          cartCtx.mapID(item);
+        }
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }, [email, cartCtx]);
 
   return (
     <CartProvider>
@@ -65,51 +69,59 @@ console.log(authCtx.isLoggedIn)
       {authCtx.isLoggedIn && <Header onShowCart={showCartHandler} />}
       <h1 className={classes.h1}> The Generics </h1>
       <main>
-        <Suspense fallback={<div className="centered">Loading....</div>}>
-        <Switch>
-        {authCtx.isLoggedIn &&(<Route path="/" exact>
-            <Home/>
-          </Route>)}
-          <Route path="/" exact>
-            <AuthForm />
-          </Route>
-          {!authCtx.isLoggedIn && (
-            <Route path="/auth"exact>
+        <Suspense
+          fallback={
+            <div className="centered">
+              <LoadingSpinner />
+            </div>
+          }
+        >
+          <Switch>
+            {authCtx.isLoggedIn && (
+              <Route path="/" exact>
+                <Home />
+              </Route>
+            )}
+            <Route path="/" exact>
               <AuthForm />
             </Route>
-          )}
-          {authCtx.isLoggedIn && (
-            <Route path="/store" exact>
-              <Products />
-            </Route>
-          )}
+            {!authCtx.isLoggedIn && (
+              <Route path="/auth" exact>
+                <AuthForm />
+              </Route>
+            )}
+            {authCtx.isLoggedIn && (
+              <Route path="/store" exact>
+                <Products />
+              </Route>
+            )}
 
-          {authCtx.isLoggedIn && (
-            <Route path="/about">
-              <About />
+            {authCtx.isLoggedIn && (
+              <Route path="/about">
+                <About />
+              </Route>
+            )}
+            {authCtx.isLoggedIn && (
+              <Route path="/" exact>
+                <Home />
+              </Route>
+            )}
+            <Route path="/contact_us">
+              <Contact />
             </Route>
-          )}
-          {authCtx.isLoggedIn && (
-            <Route path="/" exact>
-              <Home />
+
+            <Route path="/products/:product_id">
+              <ProductDetail />
             </Route>
-          )}
-          <Route path="/contact_us">
-            <Contact />
-          </Route>
 
-          <Route path="/products/:product_id">
-            <ProductDetail />
-          </Route>
-
-          <Route path="*">
-            <Redirect to="/"></Redirect>
-          </Route>
-        </Switch>
+            <Route path="*">
+              <Redirect to="/"></Redirect>
+            </Route>
+          </Switch>
         </Suspense>
       </main>
-      <Footer/>
-      </CartProvider>
+      <Footer />
+    </CartProvider>
   );
 }
 
